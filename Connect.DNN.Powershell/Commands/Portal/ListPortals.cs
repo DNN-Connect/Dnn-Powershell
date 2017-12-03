5d45fd0a8fc58fa1a3734bf28eeaba66b8e90f20
Peter Donker <peter@bring2mind.net>
2017-12-03 14:31:32 +0100

    First portal-based commands

---
+++ Connect.DNN.Powershell/Commands/Portal/ListPortals.cs
@@ -0,0 +1,27 @@
+﻿using Connect.DNN.Powershell.Framework;
+using Connect.DNN.Powershell.Framework.Models;
+using System.Management.Automation;
+
+namespace Connect.DNN.Powershell.Commands.Portal
+{
+    [Cmdlet("List", "Portals")]
+    public class ListPortals : DnnPromptCmdLet
+    {
+        protected override void ProcessRecord()
+        {
+            if (!FindSite()) { return; };
+            WriteVerbose(string.Format("list-portals on {0}", CmdSite.Url));
+            var response = DnnPromptController.ProcessCommand(CmdSite, 5, "list-portals");
+            WriteVerbose(string.Format("Retrieved response {0}", response.Status));
+            if (response.Status == ServerResponseStatus.Success)
+            {
+                var result = Newtonsoft.Json.JsonConvert.DeserializeObject<ConsoleResultModel<Models.PortalBase>>(response.Contents);
+                WriteObject(result.Data);
+            }
+            else
+            {
+                WriteObject(response.Status);
+            }
+        }
+    }
+}
