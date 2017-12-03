169f13602b8fa2cda4f31d69e345fe7031793619
Peter Donker <peter@bring2mind.net>
2017-12-03 14:31:51 +0100

    Get roles

---
+++ Connect.DNN.Powershell/Commands/Roles/ListRoles.cs
@@ -0,0 +1,30 @@
+﻿using Connect.DNN.Powershell.Framework;
+using Connect.DNN.Powershell.Framework.Models;
+using System.Management.Automation;
+
+namespace Connect.DNN.Powershell.Commands.Roles
+{
+    [Cmdlet("List", "Roles")]
+    public class ListRoles : DnnPromptCmdLet
+    {
+        [Parameter(Mandatory = false)]
+        public int PortalId { get; set; } = -1;
+
+        protected override void ProcessRecord()
+        {
+            if (!FindSite()) { return; };
+            WriteVerbose(string.Format("list-roles on {0} portal {1}", CmdSite.Url, PortalId));
+            var response = DnnPromptController.ProcessCommand(CmdSite, PortalId, 5, "list-roles");
+            WriteVerbose(string.Format("Retrieved response {0}", response.Status));
+            if (response.Status == ServerResponseStatus.Success)
+            {
+                var result = Newtonsoft.Json.JsonConvert.DeserializeObject<ConsoleResultModel<Models.RoleBase>>(response.Contents);
+                WriteObject(result.Data);
+            }
+            else
+            {
+                WriteObject(response.Status);
+            }
+        }
+    }
+}
