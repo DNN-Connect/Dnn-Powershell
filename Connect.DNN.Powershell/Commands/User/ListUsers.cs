1d870f9ecd4624199e879d6ffd80ef3efcef6094
Joe Brinkman <joe@dnncorp.com>
2018-02-03 19:14:59 -0800

    Update List-user cmdlet to support pipelines
    Fix Issue #2
    This update loops through the individual array members and outputs each record into the pipeline.  This ensures that the results are consistent with the way cmdlets were designed to operate.

--- Connect.DNN.Powershell/Commands/User/ListUsers.cs
+++ Connect.DNN.Powershell/Commands/User/ListUsers.cs
@@ -1,4 +1,5 @@
 ﻿using Connect.DNN.Powershell.Core.Commands;
+using Connect.DNN.Powershell.Core.Models;
 using Connect.DNN.Powershell.Framework.Models;
 using System.Management.Automation;
 
@@ -28,7 +29,10 @@ namespace Connect.DNN.Powershell.Commands.User
             if (CmdSite == null || CmdPortal == null) { return; };
             WriteVerbose(string.Format("list-users on {0} portal {1}", CmdSite.Url, CmdPortal.PortalId));
             var response = UserCommands.ListUsers(CmdSite, CmdPortal.PortalId, Email, Username, Role, Page, Max);
-            WriteObject(response);
+            foreach (UserModelBase user in response)
+            {
+                WriteObject(user);
+            }
         }
     }
 }
